f2f8be3d0705c0c7048761efd01b042d7bad7853
Jaime Bowen <jaime@MacBook-Pro-de-Jaime.local>
2020-12-10 18:57:29 +0100

    Añadido esquema general de funcion, a la espera del IMU

--- main.ino
+++ main.ino
@@ -6,10 +6,7 @@ Script principal para el diribile
 */
 #include <propulsion/prop.h>
 #include <guiado/guide.h>
-#include <NewPing.h>
-#include "I2Cdev.h"
-#include "MPU6050.h"
-#include "Wire.h"
+
 
 int motor_dcha = 13;
 int motor_izq = 12;
@@ -22,12 +19,51 @@ void setup(){
 }
  
 void loop(){
-    
+    int i = 0;
+    while (i <4){
+        if(i ==0) {
+            getPosition();
+            lookforObstacles();
+            mover_motor(motor = "t",time = 8) // se mueve durante 8s, y luego se para
+            getPosition();
+        }
+        i = i+1;
+        if (i==1){
+            getPosition();
+            lookforObstacles();
+            mover_motor(motor = "d",time = 5); // gira a la izquierda
+            mover_motor(motor = "t", time = 5); // se va recto
+            getPosition();
+
+        }
+        i = i +1;
+        if (i ==2){
+            getPosition();
+            lookforObstacles();
+            mover_motor(motor = "i", time = 8);
+            mover_motor(motor = "t",time = 5);
+            getPosition();
+
+        }
+        i = i +1;
+        if (i == 3){
+            getPosition();
+            lookforObstacles();
+            mover_motor(motor = "d", time = 4);
+            mover_motor(motor = "t",time = 5);
+            getPosition();
+
+        }
+        i = i +1;
 
 
+    }
+    makeEndSound();
 
 }
 
+}
+/*
 bool mover_motor(str motor){
     // Funcion sin regulación de potencia
     int motor_dcha = 13;
@@ -41,3 +77,4 @@ bool mover_motor(str motor){
     }
     
 }
+*/
